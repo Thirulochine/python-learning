69d475b130d7b30be84fe952bca3aebdb036cd29
Thirulochine <thirulochine@gmail.com>
2026-07-03 19:04:42 +0530

    Added eg2_operators in function folder

---
+++ function/eg2_operators.py
@@ -0,0 +1,28 @@
+def add():
+    print("Addition")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print("Sum is : ",a+b)
+    
+def sub():
+    print("Substraction")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print("Difference is : ",a-b)
+    
+def mul():
+    print("Multiplication")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print("Product is : ",a*b)
+    
+def div():
+    print("Division")
+    a=int(input("Enter first number:"))
+    b=int(input("Enter second number:"))
+    print("Quotient is : ",a/b)
+    
+add()
+sub()
+mul()
+div()
