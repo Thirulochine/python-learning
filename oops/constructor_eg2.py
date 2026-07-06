edaaa20f043e965985ef2f17d9e96c3d2dbd0ae2
Thirulochine <thirulochine@gmail.com>
2026-07-06 19:31:59 +0530

    modified the constructor 2nd examplle

--- oops/constructor_eg2.py
+++ oops/constructor_eg2.py
@@ -1,10 +1,13 @@
 class Student:
-    def __init__(self):
-        self.name = "Shastike"
-        self.register_number = "123456"
+    def __init__(self,name,register_number):
+        self.name = name
+        self.register_number = register_number
     def display(self):
         print("Name:", self.name)
         print("Register Number:", self.register_number)
 
-obj=Student()
-obj.display()
+obj1=Student("Shastika", "123456")           #constructor is called when object is created
+obj2=Student("John", "654321")
+
+obj1.display()
+obj2.display()
