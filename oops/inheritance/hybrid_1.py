fe548beb8edd1cdf9a4b892e98187359e347b855
Thirulochine <thirulochine@gmail.com>
2026-07-08 20:41:16 +0530

    added hybrid inheritance

---
+++ oops/inheritance/hybrid_1.py
@@ -0,0 +1,34 @@
+#4. Hybrid Inheritance
+class dad():
+    def money (self):
+        print("dad has money")                  #combination of many type of inheritance
+        
+class land():
+    def important(self):
+        print("important land")
+               
+class son1(dad,land):
+    def phone (self):
+        print("son 1 has iphone")
+
+class son2(dad):
+    def phone(self):
+        print("son 2 has samsung phone")
+        
+class son3(dad):
+    def phone(self):
+        print("son 3 has vivo phone")
+        
+s1=son1()
+s2=son2()
+s3=son3()
+
+s1.money()
+s1.phone()
+s1.important()
+
+s2.money()
+s2.phone()
+
+s3.money()
+s3.phone()
