f1b71bb340c4c9a4f78232bd46e5ea35f51efa41
Thirulochine <thirulochine@gmail.com>
2026-07-08 19:39:42 +0530

    inheritance problems

---
+++ oops/inheritance/single_inheritance_1.py
@@ -0,0 +1,12 @@
+class dad:
+    def obj(self):
+        print("this is dad mobile")
+        
+class son (dad):
+    def lap(self):
+        print("this is son laptop")
+        
+ram=son()
+ram.obj()
+ram.lap()
+    
