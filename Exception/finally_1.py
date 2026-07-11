70fd3031659667540cfe3326c1e95c280f24e1b8
Thirulochine <thirulochine@gmail.com>
2026-07-11 18:30:20 +0530

    Finally keyword

---
+++ Exception/finally_1.py
@@ -0,0 +1,15 @@
+try:
+    a=int(input("Enter a : "))
+    b=int(input("Enter b : "))
+    c=input()
+    
+    print(c/a)
+
+except ValueError as e:
+    print(e)
+    
+except TypeError as e:
+    print("Type Error" ,e)
+    
+finally:
+    print("everything is done")
